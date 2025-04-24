e3ca608d87e51a13693051ce46defd26bc96a730
HendersonA <henderson_aryel@hotmail.com>
2025-04-23 22:31:17 -0300

    Add wave system

---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using static ObjectPool;
+
+public class WaveManager : MonoBehaviour
+{
+    [Serializable]
+    public struct Wave
+    {
+        public GameObject[] _prefabs;
+    }
+    [SerializeField] private Wave[] _waves;
+    public Action OnStartWave;
+    private List<GameObject> _cachedObjects;
+    private int _currentWaveIndex = -1;
+
+    private void Start()
+    {
+        _cachedObjects = new List<GameObject>();
+        SpawnWave();
+        OnStartWave += SpawnWave;
+    }
+    [ContextMenu("Spawn Wave")]
+    public void SpawnWave()
+    {
+        _currentWaveIndex++;
+        if (_currentWaveIndex > _waves.Length - 1) _currentWaveIndex = 0;
+
+        var wave = _waves[_currentWaveIndex];
+        for (int j = 0; j < wave._prefabs.Length; j++)
+        {
+            var prefab = wave._prefabs[j];
+            SpawnFromPool(prefab);
+        }
+    }
+    private void SpawnFromPool(GameObject prefabObject)
+    {
+        GameObject poolObject = ObjectPool.Instance.Get(prefabObject);
+
+        if (poolObject != null)
+        {
+            Poolable poolable = poolObject.GetComponent<Poolable>();
+            if (poolable != null)
+                poolable.Prefab = prefabObject;
+            else
+            {
+                poolable = poolObject.AddComponent<Poolable>();
+                poolable.Prefab = prefabObject;
+            }
+            poolObject.SetActive(true);
+            _cachedObjects.Add(poolObject);
+        }
+    }
+}
