b221abefb4f159df705bca5dccfa2f460ba809dc
HendersonA <henderson_aryel@hotmail.com>
2025-04-25 03:03:04 -0300

    Refactored sprite animations script

---
+++ Assets/Scripts/SpriteAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteAnimation : MonoBehaviour
+{
+    [SerializeField] private Sprite[] _sprites;
+    private SpriteRenderer _spriteRenderer;
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void StartAnimation() => StartCoroutine(SpriteAnimationCoroutine());
+    private IEnumerator SpriteAnimationCoroutine()
+    {
+        for (int i = 0; i < _sprites.Length; i++)
+        {
+            var sprite = _sprites[i];
+            _spriteRenderer.sprite = sprite;
+            yield return new WaitForSeconds(.1f);
+        }
+        _spriteRenderer.sprite = null;
+        yield return new WaitForEndOfFrame();
+    }
+}
